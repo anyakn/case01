df6ad2fb2acd1aa58505bce7ef57dc5779e721e0
anyaknopova <annilik22@gmail.com>
2023-03-05 22:53:58 +0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+from turtle import*
+pu()
+
+goto(-300,300)
+shape('turtle')
+bgcolor('navy')
+pd()
+for i in range(4):
+    fd(600)
+    rt(90)
+rt(90)
+circle(50,180)
+circle(50,180)
+
+pu()
+done()
