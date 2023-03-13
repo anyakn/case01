b1915ce645e6c86324e0947416a51ff5fb106662
anyaknopova <annilik22@gmail.com>
2023-03-13 18:58:57 +0700

    done

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
+'''
+Кнопова Анна - 60
+Балан Каролина - 40
+Шилкова Ульяна - 40
+'''
+
+
 from turtle import *
 import bottom_right_anyas_part as br
 import bottom_left_karolinas_part as bl
 import top_left_common_part as tl
 import top_right_ulianas_part as tr
+
 pu()
 speed(-1)
 
